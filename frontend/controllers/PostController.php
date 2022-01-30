0f33fd6a5af007c34a2aec172e65dbfd448a5006
YahorDanchanka <danchenko.dev@yandex.by>
2022-01-30 19:37:13 +0300

    CORS enabled

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -3,8 +3,20 @@
 namespace frontend\controllers;
 
 use common\models\Post;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class PostController extends ActiveController {
     public $modelClass = Post::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['corsFilter'] = [
+            'class' => Cors::class
+        ];
+
+        return $behaviors;
+    }
 }
